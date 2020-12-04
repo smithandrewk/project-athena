a9588b73f0eb33c6c9b08c98ac7128c71ad1ae6a
Andrew <andrewsmith1025@gmail.com>
2020-12-04 16:00:15 -0500

    refinements

--- Task2/scripts/eval_model.py
+++ Task2/scripts/eval_model.py
@@ -14,6 +14,71 @@ from utils.metrics import error_rate, get_corrections
 from models.athena import Ensemble, ENSEMBLE_STRATEGY
 
 
+def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1, legend=True):
+    """Draws a bar plot with multiple bars per data point.
+
+    Parameters
+    ----------
+    ax : matplotlib.pyplot.axis
+        The axis we want to draw our plot on.
+
+    data: dictionary
+        A dictionary containing the data we want to plot. Keys are the names of the
+        data, the items is a list of the values.
+
+        Example:
+        data = {
+            "x":[1,2,3],
+            "y":[1,2,3],
+            "z":[1,2,3],
+        }
+
+    colors : array-like, optional
+        A list of colors which are used for the bars. If None, the colors
+        will be the standard matplotlib color cyle. (default: None)
+
+    total_width : float, optional, default: 0.8
+        The width of a bar group. 0.8 means that 80% of the x-axis is covered
+        by bars and 20% will be spaces between the bars.
+
+    single_width: float, optional, default: 1
+        The relative width of a single bar within a group. 1 means the bars
+        will touch eachother within a group, values less than 1 will make
+        these bars thinner.
+
+    legend: bool, optional, default: True
+        If this is set to true, a legend will be added to the axis.
+    """
+
+    # Check if colors where provided, otherwhise use the default color cycle
+    if colors is None:
+        colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    # Number of bars per group
+    n_bars = len(data)
+
+    # The width of a single bar
+    bar_width = total_width / n_bars
+
+    # List containing handles for the drawn bars, used for the legend
+    bars = []
+
+    # Iterate over all data
+    for i, (name, values) in enumerate(data.items()):
+        # The offset in x direction of that bar
+        x_offset = (i - n_bars / 2) * bar_width + bar_width / 2
+
+        # Draw a bar for every value of that type
+        for x, y in enumerate(values):
+            bar = ax.bar(x + x_offset, y, width=bar_width * single_width, color=colors[i % len(colors)])
+
+        # Add a handle to the last drawn bar, which we'll need for the legend
+        bars.append(bar[0])
+
+    # Draw legend if we need
+    if legend:
+        ax.legend(bars, data.keys())
+
 def evaluate_cnn(trans_configs, model_configs,
                  data_configs, save=False, output_dir=None):
     """
@@ -109,29 +174,101 @@ def evaluate_cnn(trans_configs, model_configs,
 
         # TODO: collect and dump the evaluation results to file(s) such that you can analyze them later.
         print(">>> Evaluations on [{}]:\n{}".format(ae_file, results))
-    plt.bar(ae_list,UM)
-    plt.title("Evaluation against undefended model")
+    FGSM_rotation = [ae_list[0],ae_list[2],ae_list[4]]
+    FGSM_rotation_UM = [UM[0],UM[2],UM[4]]
+    FGSM_rotation_e = [e[0],e[2],e[4]]
+    FGSM_rotation_PGDADT = [PGDADT[0],PGDADT[2],PGDADT[4]]
+    FGSM_translation = [ae_list[1],ae_list[3],ae_list[5]]
+    FGSM_translation_UM = [UM[1],UM[3],UM[5]]
+    FGSM_translation_e = [e[1],e[3],e[5]]
+    FGSM_translation_PGDADT = [PGDADT[1],PGDADT[3],PGDADT[5]]
+    PGD_rotation = [ae_list[6],ae_list[8],ae_list[10]]
+    PGD_rotation_UM = [UM[6],UM[8],UM[10]]
+    PGD_rotation_e = [e[6],e[8],e[10]]
+    PGD_rotation_PGDADT = [PGDADT[6],PGDADT[8],PGDADT[10]]
+    PGD_translation = [ae_list[7],ae_list[9],ae_list[11]]
+    PGD_translation_UM = [UM[7],UM[9],UM[11]]
+    PGD_translation_e = [e[7],e[9],e[11]]
+    PGD_translation_PGDADT = [PGDADT[7],PGDADT[9],PGDADT[11]]
+    ##
+    data = {
+        "UM": FGSM_rotation_UM,
+        "Ensemble": FGSM_rotation_e,
+        "PGD-ADT": FGSM_rotation_PGDADT
+    }
+
+    fig, ax = plt.subplots()
+    bar_plot(ax, data, total_width=.8, single_width=.9)
+    plt.xticks(range(3),FGSM_rotation)
     plt.ylabel("Error")
-    plt.xticks(rotation=45,fontsize=8)
     plt.gcf().set_size_inches(15, 10)
-    plt.savefig('undefended.jpg',dpi=100,bbox_inches='tight')
+    plt.savefig('FGSM_rotation.jpg',dpi=100,bbox_inches='tight')
+    plt.show()
+    ##
+    data = {
+        "UM": FGSM_translation_UM,
+        "Ensemble": FGSM_translation_e,
+        "PGD-ADT": FGSM_translation_PGDADT
+    }
+
+    fig, ax = plt.subplots()
+    bar_plot(ax, data, total_width=.8, single_width=.9)
+    plt.xticks(range(3),FGSM_translation)
+    plt.ylabel("Error")
+    plt.gcf().set_size_inches(15, 10)
+    plt.savefig('FGSM_translation.jpg',dpi=100,bbox_inches='tight')
+    plt.show()
+    ##3
+    data = {
+        "UM": PGD_rotation_UM,
+        "Ensemble": PGD_rotation_e,
+        "PGD-ADT": PGD_rotation_PGDADT
+    }
+    fig, ax = plt.subplots()
+    bar_plot(ax, data, total_width=.8, single_width=.9)
+    plt.xticks(range(3),PGD_rotation)
+    plt.ylabel("Error")
+    plt.gcf().set_size_inches(15, 10)
+    plt.savefig('PGD_rotation.jpg',dpi=100,bbox_inches='tight')
+    plt.show()
+    ##
+    data = {
+        "UM": PGD_translation_UM,
+        "Ensemble": PGD_translation_e,
+        "PGD-ADT": PGD_translation_PGDADT
+    }
+
+    fig, ax = plt.subplots()
+    bar_plot(ax, data, total_width=.8, single_width=.9)
+    plt.xticks(range(3),PGD_translation)
+    plt.ylabel("Error")
+    plt.gcf().set_size_inches(15, 10)
+    plt.savefig('PGD_translation.jpg',dpi=100,bbox_inches='tight')
     plt.show()
 
-    plt.bar(ae_list,e)
-    plt.title("Evaluation against vanilla athena model")
-    plt.ylabel("Error")
-    plt.xticks(rotation=45,fontsize=8)
-    plt.gcf().set_size_inches(15, 10)
-    plt.savefig('vanilla.jpg',dpi=100,bbox_inches='tight',)
-    plt.show()
-
-    plt.bar(ae_list,PGDADT)
-    plt.title("Evaluation against PGD-ADT model")
-    plt.ylabel("Error")
-    plt.xticks(rotation=45,fontsize=8)
-    plt.gcf().set_size_inches(15, 10)
-    plt.savefig('pgd-adt.jpg',dpi=100,bbox_inches='tight')
-    plt.show()
+#    plt.bar(ae_list,UM)
+#    plt.title("Evaluation against undefended model")
+#    plt.ylabel("Error")
+#    plt.xticks(rotation=45,fontsize=8)
+#    plt.gcf().set_size_inches(15, 10)
+#    plt.savefig('undefended.jpg',dpi=100,bbox_inches='tight')
+#    plt.show()
+#
+#    plt.bar(ae_list,e)
+#    plt.title("Evaluation against vanilla athena model")
+#    plt.ylabel("Error")
+#    plt.xticks(rotation=45,fontsize=8)
+#    plt.gcf().set_size_inches(15, 10)
+#    plt.savefig('vanilla.jpg',dpi=100,bbox_inches='tight',)
+#    plt.show()
+#
+#    plt.bar(ae_list,PGDADT)
+#    plt.title("Evaluation against PGD-ADT model")
+#    plt.ylabel("Error")
+#    plt.xticks(rotation=45,fontsize=8)
+#    plt.gcf().set_size_inches(15, 10)
+#    plt.savefig('pgd-adt.jpg',dpi=100,bbox_inches='tight')
+#    plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
